b885896c75a599c9d1bccaf3dee665b3693a0a6a
newObjectccc <897333949@qq.com>
2024-02-25 02:11:00 +0800

    feat: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
-import reactLogo from './assets/react.svg'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  useEffect(() => {
+    console.log('App rendereds', chrome.tabs);
+    chrome.tabs.query({currentWindow: true}, (tabs) => {
+      console.log("🚀 ~ chrome.tabs.query ~ tabs:", tabs)
+    });
+  }, [])
+  
   return (
     <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -21,9 +21,6 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
