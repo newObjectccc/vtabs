7cd5e228999100f1ced31e844b23014eae433e9d
newObjectccc <897333949@qq.com>
2024-03-01 18:41:31 +0800

    feat: draggable

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Image } from '@nextui-org/react';
+import { motion } from 'framer-motion';
 import { useEffect, useState } from 'react';
+import { DragDropContext, Draggable, DropResult, Droppable } from 'react-beautiful-dnd';
 import './App.css';
 
 function App() {
@@ -46,21 +48,61 @@ function App() {
     };
   }, []);
 
+  const onDragEnd = (result: DropResult) => {
+    if (!result.destination) return;
+    const items = Array.from(tabList!);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+    setTabList(items);
+  };
+
   return (
-    <ul className="p-2 h-lvh w-lvw text-slate-50 bg-[#3B3B3B] overflow-y-auto">
-      {tabList?.map((tab) => (
-        <li
-          className={`flex flex-row items-center flex-1 rounded-md mb-1 p-2 hover:bg-[#454545] ${tab.active ? 'bg-[#4D4D4D] shadow-md shadow-slate-950' : ''}`}
-          onClick={() => changeActiveTab(tab.id!)}
-        >
-          <Image alt="nextui logo" height={18} radius="sm" src={tab.favIconUrl} width={18} />
-          <div className={`ml-2 flex-1 pr-2 cursor-default ${tab.active ? '' : 'text-blur'}`}>
-            {tab.title}
-          </div>
-          <div className="close" onClick={() => removeTab(tab.id!)}></div>
-        </li>
-      ))}
-    </ul>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="droppable">
+        {(provided) => (
+          <ul
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            className="p-2 h-lvh w-lvw flex flex-col items-center text-slate-50 bg-[#3B3B3B] overflow-y-auto"
+          >
+            {tabList?.map((tab, index) => (
+              <Draggable key={tab.id!.toString()} draggableId={tab.id!.toString()} index={index}>
+                {(provided) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    className={`w-11/12`}
+                    onClick={() => changeActiveTab(tab.id!)}
+                  >
+                    <motion.li
+                      className={`flex flex-row items-center rounded-md mb-1 p-2 wrapper hover:bg-[#454545] ${
+                        tab.active ? 'bg-[#4D4D4D] shadow-md shadow-slate-950' : ''
+                      }`}
+                      whileHover={{ scale: 1.11 }}
+                      whileTap={{ scale: 1.05 }}
+                    >
+                      <Image
+                        alt="nextui logo"
+                        height={18}
+                        radius="sm"
+                        src={tab.favIconUrl}
+                        width={18}
+                      />
+                      <div className={`flex-1 mx-2 cursor-grab ${tab.active ? '' : 'text-blur'}`}>
+                        {tab.title}
+                      </div>
+                      <div className="close" onClick={() => removeTab(tab.id!)}></div>
+                    </motion.li>
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </ul>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 }
 
