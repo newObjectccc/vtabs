cdb72e33ab8321f5de923847045f43bafeb40046
newObjectccc <897333949@qq.com>
2024-03-01 15:38:37 +0800

    feat: fullscreen

--- public/background.js
+++ public/background.js
@@ -2,4 +2,7 @@
 
 chrome.action.onClicked.addListener((tab) => {
   chrome.sidePanel.open({ windowId: tab.windowId });
+  chrome.windows.getCurrent(function (window) {
+    chrome.windows.update(window.id, { state: 'fullscreen' });
+  });
 });
