cd132be92c47f32f6e37501e1cc3a3095d3ca55c
newObjectccc <897333949@qq.com>
2024-03-02 19:05:21 +0800

    fix: prevent rearrange when tab update

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { DragDropContext, Draggable, DropResult, Droppable } from 'react-beautif
 import './App.css';
 
 function App() {
-  const [tabList, setTabList] = useState<chrome.tabs.Tab[]>();
+  const [tabList, setTabList] = useState<chrome.tabs.Tab[]>([]);
 
   const changeActiveTab = (tabId: number) => {
     chrome.tabs.update(tabId, { active: true });
@@ -24,16 +24,29 @@ function App() {
     const onTabsUpdated = (_tabId: number, changeInfo: chrome.tabs.TabChangeInfo) => {
       if (changeInfo.status === 'complete') {
         tabs.query({ currentWindow: true }, (tabs) => {
-          setTabList(tabs);
+          setTabList((prev) => {
+            // if new tab is opened
+            if (prev.length < tabs.length) return prev.concat([tabs.find((t) => t.id === _tabId)!]);
+            // if tab is updated
+            return prev.map((tab) =>
+              tab.id === _tabId ? tabs.find((t) => t.id === _tabId)! : tab
+            );
+          });
         });
       }
     };
     const onTabsRemoved = (_tabId: number) => {
       setTabList((prev) => prev?.filter((tab) => tab.id !== _tabId));
     };
-    const onTabsActived = () => {
-      tabs.query({ currentWindow: true }, (tabs) => {
-        setTabList(tabs);
+    const onTabsActived = (activeInfo: { tabId: number; windowId: number }) => {
+      tabs.query({ currentWindow: true }, () => {
+        setTabList((prev) =>
+          prev.map((tab) => {
+            tab.active = false;
+            if (tab.id === activeInfo.tabId) tab.active = true;
+            return tab;
+          })
+        );
       });
     };
 
@@ -46,6 +59,7 @@ function App() {
       tabs.onRemoved.removeListener(onTabsRemoved);
       tabs.onActivated.removeListener(onTabsActived);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const onDragEnd = (result: DropResult) => {
@@ -89,7 +103,9 @@ function App() {
                         src={tab.favIconUrl}
                         width={18}
                       />
-                      <div className={`flex-1 mx-2 cursor-grab ${tab.active ? '' : 'text-blur'}`}>
+                      <div
+                        className={`flex-1 mx-2 overflow-hidden text-ellipsis cursor-grab ${tab.active ? '' : 'text-blur'}`}
+                      >
                         {tab.title}
                       </div>
                       <div className="close" onClick={() => removeTab(tab.id!)}></div>
